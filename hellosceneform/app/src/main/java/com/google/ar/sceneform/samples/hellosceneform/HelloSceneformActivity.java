4207993b8d2d33a0d281b1029e589eb4f4d42bb3
itan <itanbp@gmail.com>
2018-05-15 12:59:20 +0300

    scale animation for model - works only with AnchorNode, doesn't work with TransformableNode

--- hellosceneform/app/src/main/java/com/google/ar/sceneform/samples/hellosceneform/HelloSceneformActivity.java
+++ hellosceneform/app/src/main/java/com/google/ar/sceneform/samples/hellosceneform/HelloSceneformActivity.java
@@ -18,6 +18,7 @@ package com.google.ar.sceneform.samples.hellosceneform;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.MotionEvent;
 import android.widget.Toast;
@@ -25,7 +26,12 @@ import com.google.ar.core.Anchor;
 import com.google.ar.core.HitResult;
 import com.google.ar.core.Plane;
 import com.google.ar.core.Plane.Type;
+import com.google.ar.core.TrackingState;
 import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.FrameTime;
+import com.google.ar.sceneform.Scene;
+import com.google.ar.sceneform.math.Vector3;
+import com.google.ar.sceneform.math.Vector3Evaluator;
 import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.ScaleController;
@@ -39,6 +45,10 @@ public class HelloSceneformActivity extends AppCompatActivity {
 
   private ArFragment arFragment;
   private ModelRenderable andyRenderable;
+  //private TransformableNode transformableNode;
+  private AnchorNode anchorNode;
+  private float prevTime = -1;
+  private int index = 0;
 
   @Override
   @SuppressWarnings({"AndroidApiChecker", "FutureReturnValueIgnored"})
@@ -54,7 +64,7 @@ public class HelloSceneformActivity extends AppCompatActivity {
     // When you build a Renderable, Sceneform loads its resources in the background while returning
     // a CompletableFuture. Call thenAccept(), handle(), or check isDone() before calling get().
     ModelRenderable.builder()
-        .setSource(this, Uri.parse("Octopus_eyes.sfb"))
+        .setSource(this, Uri.parse("Octopus.sfb"))
         .build()
         .thenAccept(renderable -> andyRenderable = renderable)
         .exceptionally(
@@ -78,18 +88,65 @@ public class HelloSceneformActivity extends AppCompatActivity {
 
           // Create the Anchor.
           Anchor anchor = hitResult.createAnchor();
-          AnchorNode anchorNode = new AnchorNode(anchor);
+            anchorNode = new AnchorNode(anchor);
           anchorNode.setParent(arFragment.getArSceneView().getScene());
 
           // Create the transformable andy and add it to the anchor.
-          TransformableNode andy = new TransformableNode(arFragment.getTransformationSystem());
-          andy.setParent(anchorNode);
-          ScaleController scaleController = andy.getScaleController();
-          scaleController.setMaxScale(3f);
-          scaleController.setElasticity(1f);
-          andy.setRenderable(andyRenderable);
-          andy.select();
+            /*
+            transformableNode = new TransformableNode(arFragment.getTransformationSystem());
+            transformableNode.setParent(anchorNode);
+            ScaleController scaleController = transformableNode.getScaleController();
+            scaleController.setMaxScale(3f);
+            scaleController.setElasticity(1f);
+            transformableNode.setRenderable(andyRenderable);
+            transformableNode.select();
+            */
+            anchorNode.setRenderable(andyRenderable);
 
         });
+
+
+
+    arFragment.getArSceneView().getScene().setOnUpdateListener(new Scene.OnUpdateListener() {
+        @Override
+        public void onUpdate(FrameTime frameTime) {
+
+            // Let the fragment update its state first.
+            arFragment.onUpdate(frameTime);
+
+            // If there is no frame then don't process anything.
+            if (arFragment.getArSceneView().getArFrame() == null) {
+                return;
+            }
+
+            // If ARCore is not tracking yet, then don't process anything.
+            if (arFragment.getArSceneView().getArFrame().getCamera().getTrackingState() != TrackingState.TRACKING) {
+                return;
+            }
+
+            if (index == 0) {
+                prevTime = frameTime.getStartSeconds();
+                index++;
+            }
+
+
+            if (anchorNode != null && frameTime.getStartSeconds() - prevTime > 0.3f) {
+
+                float fraction = index % 100;
+                fraction = fraction / 100;
+                index++;
+
+                prevTime = frameTime.getStartSeconds();
+
+                Vector3Evaluator evaluator = new Vector3Evaluator();
+                Vector3 animationVector = evaluator.evaluate(fraction, new Vector3(1,1,1), new Vector3(4,4,4));
+                anchorNode.setLocalScale(animationVector);
+
+                Log.v("MOTEK", "animationVector");
+            }
+
+        }
+    });
+
   }
 }
